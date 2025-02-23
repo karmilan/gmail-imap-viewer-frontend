22d050b7592c75e307521acd366f454e3005c5cc
karmilan <delftkarmilan@gmail.com>
2025-02-23 23:54:42 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="text-5xl bg-red-400 text-yellow-700">hello world</div>
+    </>
+  );
+}
+
+export default App;
